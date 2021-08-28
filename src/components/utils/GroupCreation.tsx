4b528232da001b9c50a6a001624944527815d126
Diego de Jesús Ramírez Rodríguez <diegojrr00@gmail.com>
2021-08-28 15:54:02 -0500

    Basic group creation modal

---
+++ src/components/utils/GroupCreation.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Button, Modal } from "native-base";
+import { useState } from "react";
+
+const GroupCreationModal = () => {
+    const [showModal, setShowModal] = useState(true)
+
+    return (
+    <Modal isOpen={showModal} size={"full"} onClose={() => setShowModal(false)}>
+        <Modal.Content {...styles["bottom"]}>
+          <Modal.CloseButton />
+          <Modal.Header>Nuevo grupo</Modal.Header>
+          <Modal.Body>
+            Test modal body
+          </Modal.Body>
+          <Modal.Footer>
+            <Button.Group variant="ghost" space={2}>
+              <Button>
+                Crear
+              </Button>
+            </Button.Group>
+          </Modal.Footer>
+        </Modal.Content>
+      </Modal>
+    )
+}
+
+const styles = {
+    bottom: {
+        marginBottom: 0,
+        marginTop: "auto",
+    }, 
+    center: {},
+}
+export default GroupCreationModal;
