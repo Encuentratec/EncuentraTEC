cb8e4a0df9ef8fa43d28902a2ac8b221d2e8686a
Alam Sanchez <asancheztorres1905@gmail.com>
2021-08-28 22:42:51 -0400

    Add redux config

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { ThemeProvider } from "react-native-rapi-ui";
-import Navigation from "./src/navigation";
-import { AuthProvider } from "./src/provider/AuthProvider";
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { ThemeProvider } from 'react-native-rapi-ui';
+import { Provider } from 'react-redux';
+import Navigation from './src/navigation';
+import { AuthProvider } from './src/provider/AuthProvider';
+import store from './src/redux/store';
 
 export default function App() {
-  const images = [
-    require("./assets/images/login.png"),
-    require("./assets/images/register.png"),
-    require("./assets/images/forget.png"),
-  ];
-  return (
-    <ThemeProvider images={images}>
-      <AuthProvider>
-        <Navigation />
-      </AuthProvider>
-      <StatusBar />
-    </ThemeProvider>
-  );
+    const images = [
+        require('./assets/images/login.png'),
+        require('./assets/images/register.png'),
+        require('./assets/images/forget.png'),
+    ];
+
+    return (
+        <Provider store={store}>
+            <ThemeProvider images={images}>
+                <AuthProvider>
+                    <Navigation />
+                </AuthProvider>
+                <StatusBar />
+            </ThemeProvider>
+        </Provider>
+    );
 }
---
+++ src/redux/slices/user.ts
@@ -0,0 +1,27 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { User } from '../../types/supabaseMapped';
+
+type SliceState = {
+    state: 'logged out' | 'loading' | 'logged in';
+    user: User | null | undefined;
+};
+
+const initialState: SliceState = { state: 'logged out', user: null };
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        setUserData: (state, action: PayloadAction<SliceState>) => {
+            if (action.payload) {
+                state = {
+                    state: 'logged in',
+                    user: action.payload.user,
+                };
+            }
+        },
+    },
+});
+
+export const { setUserData } = userSlice.actions;
+export default userSlice.reducer;
